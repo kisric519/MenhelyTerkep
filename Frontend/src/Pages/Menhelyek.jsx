6974b50cd96dd7793b122e72eddc78e05a58f743
vtunde583 <vartun583@hengersor.hu>
2025-02-13 12:16:15 +0100

    reszponzív beállítások

--- Frontend/src/Pages/Menhelyek.jsx
+++ Frontend/src/Pages/Menhelyek.jsx
@@ -11,6 +11,7 @@ const Menhelyek = () => {
                 <div className="row">
                 <div className="col-6 heroSzoveg">
                     <h1>Menhelyek</h1>
+                    <p>Ismerd meg Magyarország állatmenhelyeit</p>
                 </div>
                 <div className="col-6 heroKep">
                     <img src={menhelyoldalKep} alt="" />
